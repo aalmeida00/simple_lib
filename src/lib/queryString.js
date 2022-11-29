5fb4e429bc3004afff40fd289602a332a23498de
Andre Almeida <andre-silva78@hotmail.com>
2022-11-29 15:56:23 -0300

    Isolate the map on a private function to organize the code

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,9 +1,9 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Value cannot be an object');
+  }
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = object =>
-  Object.entries(object)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Value cannot be an object');
-      }
-      return `${key}=${value}`;
-    })
-    .join('&');
+  Object.entries(object).map(keyValueToString).join('&');
