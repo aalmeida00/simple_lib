28e133e2a810087aea1afb53b82ffb0fc5a630c8
Andre Almeida <andre-silva78@hotmail.com>
2022-11-29 15:24:18 -0300

    Add new tests to sum method

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,1 +1,7 @@
-module.exports.sum = (num1, num2) => num1 + num2
+module.exports.sum = (num1, num2) => {
+  if (Number.isNaN(parseInt(num1,10)) || Number.isNaN(parseInt(num2,10))) 
+    throw new Error('Cannot sum what is not a number')
+    
+  return +num1 + +num2
+
+}
