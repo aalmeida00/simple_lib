ed6e2a6d17d5b71f931db41fe5c69d07787a58c2
Andre Almeida <andre-silva78@hotmail.com>
2022-11-29 15:41:12 -0300

    Create a queryString converter and the first test

---
+++ src/lib/queryString.spec.js
@@ -0,0 +1,15 @@
+const { queryString } = require('./queryString')
+
+
+describe('Object to query string', () => {
+  it('should create a valid query string when an object is correct', () => {
+    const object = {
+      name: 'Andre',
+      job: 'Developer',
+    }
+
+    expect(queryString(object)).toBe('name=Andre&job=Developer')
+  })
+})
+
+// describe('Object to query string', () => {})
