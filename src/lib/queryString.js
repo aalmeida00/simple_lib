34aeb99324e56e8a443c89b40f01fe6037214004
Andre Almeida <andre-silva78@hotmail.com>
2022-11-29 15:54:43 -0300

    Create new tests to queryString function

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,4 +1,9 @@
 module.exports.queryString = object =>
   Object.entries(object)
-    .map(([key, value]) => `${key}=${value}`)
+    .map(([key, value]) => {
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        throw new Error('Value cannot be an object');
+      }
+      return `${key}=${value}`;
+    })
     .join('&');
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,6 +9,29 @@ describe('Object to query string', () => {
 
     expect(queryString(object)).toBe('name=Andre&job=Developer');
   });
+
+  it('should create a valid query string when an array is passed', () => {
+    const object = {
+      name: 'Andre',
+      abilities: ['React', 'Jest'],
+    };
+
+    expect(queryString(object)).toBe('name=Andre&abilities=React,Jest');
+  });
+
+  it('should throw an error when a object is passed as value ', () => {
+    const object = {
+      name: 'Andre',
+      abilities: {
+        frontEnd: 'React',
+        backEnd: 'Node',
+      },
+    };
+
+    expect(() => {
+      queryString(object);
+    }).toThrowError();
+  });
 });
 
 // describe('Object to query string', () => {})
